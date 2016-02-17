dd5d7125ed3618c73d54e56c4dd488cabebfd416
Jessica Koch <jessicakoch136@icloud.com>
2016-02-16 17:58:32 -0700

    remove obsolete files

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
 "use strict";
 
 import board from "./js/board";
-// import BlackTriangle from "./js/BlackTriangle";
-
-// const triangle = new BlackTriangle("#triangle");
-
-// window.setInterval(
-//   () => {
-//     triangle.rotate(1);
-//     triangle.render();
-//   },
-//   20
-// );
