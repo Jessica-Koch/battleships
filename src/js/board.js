5151ecfdbdbe7e25c816faf5a93fa850a18c76bd
Jessica Koch <jessicakoch136@icloud.com>
2016-02-24 16:33:41 -0700

    change variables within class Board to be more intuitive

--- src/js/board.js
+++ src/js/board.js
@@ -1,13 +1,14 @@
 // class expression defining board
 class Board {
     constructor(width, height) {
-        this.width = width;
-        this.height = height;
-        this.grid = new Array(this.width);
-        for (var i = 0; i < this.width; i++) {
-            this.grid[i] = new Array(height);
-            for(var j = 0; j < this.height; j++) {
-                this.grid[i][j] = {name: "(" + i + ", " + j + ")"};
+        // height and width can be changed to make a custom game, but must maintain state throughout game 
+        this._width = width;
+        this._height = height;
+        this.grid = new Array(this._width);
+        for (var x = 0; x < this._width; x++) {
+            this.grid[x] = new Array(height);
+            for(var y = 0; y < this._height; y++) {
+                this.grid[x][y] = {name: "(" + x + ", " + y + ")"};
             }
         }
         console.dir(this.grid);
@@ -26,8 +27,8 @@ class Board {
         });
     }
 }
-var gameBoard = new Board(10, 10).drawBoard();
-// board.appendChild(gameBoard);
+var gameBoard = new Board(10, 10);
+gameBoard.drawBoard();
 
 
 function Ship(id, shipName, shipSize) {
